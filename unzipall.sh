b06db5c867a51e42eca2142604fb1e992e94ed7d
benavidescuevas <benavidescuevas@users.noreply.github.com>
2019-01-11 16:33:57 +0100

    Comments on scrit file

--- unzipall.sh
+++ unzipall.sh
@@ -1,4 +1,10 @@
+#simple script to uncompress (unzip and 7z files) a set of compressed filed in a directory and generate a folder in the same directory as 
+# of the compressed file. In this case, .zip and .7z files are analyzed
+
+#for zip files
 find -name '*.zip' -exec sh -c 'unzip -d "${1%.*}" "$1"' _ {} \;
+
+#for 7z files 
 find -name '*.7z' -exec sh -c '7za x "$1" -o"${1%.*}"' _ {} \;
 
 
